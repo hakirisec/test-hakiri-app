70941724b2cf99b402a2be0972ed10e0a0bdc4f6
Vasily Vasinov <des.elyon@gmail.com>
2016-06-09 17:58:52 -0700

    Update members_controller.rb

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class MembersController < ApplicationController
   def show
-    @member = Member.where(conditions: "custom_id = '#{params[:custom_id]}'").first
+    @member = Member.where("custom_id = '#{params[:custom_id]}'").first
   end
 end
